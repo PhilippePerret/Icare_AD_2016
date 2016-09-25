ac2e32701bb34b51e0f8eecea1c2a27c910367be
Philippe Perret <philippe.perret@yahoo.fr>
2016-09-25 09:18:17 +0200

    Aménagement bureau administrateur

--- objet/bureau/lib/module/bureau_admin/bureau_admin.rb
+++ objet/bureau/lib/module/bureau_admin/bureau_admin.rb
@@ -8,13 +8,27 @@ class << self
   end
   def liens_editions
     (
-      'Édition des BASES DE DONNÉES'.in_a(href: 'database/edit').in_div +
-      'Édition des Modules d’apprentissage'.in_a(href: 'abs_module/edit').in_div +
-      'Édition des WATCHERS'.in_a(href: 'watcher/edit').in_div +
+      'BASES DE DONNÉES'.in_a(href: 'database/edit').in_div +
+      boutons_modules_apprentissage +
+      'WATCHERS'.in_a(href: 'watcher/edit').in_div +
       'Mailing list'.in_a(href: 'admin/mailing').in_div +
       'Visite le site comme…'.in_a(href: 'admin/visit_as').in_div +
-      'Check SYNCHRO'.in_a(href: 'admin/dashboard?opadmin=check_synchro').in_div
+      bouton_check_synchro
     ).in_div
+  end
+  def boutons_modules_apprentissage
+    if OFFLINE
+      'Modules d’apprentissage'.in_a(href: 'abs_module/edit').in_div
+    else
+      ''
+    end
+  end
+  def bouton_check_synchro
+    if OFFLINE
+      'Check SYNCHRO'.in_a(href: 'admin/dashboard?opadmin=check_synchro').in_div
+    else
+      ''
+    end
   end
 end #/<< self
 end #/Bureau
