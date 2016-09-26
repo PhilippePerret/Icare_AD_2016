4e95e48b79ab77819d64d225657ddfe858ff51fd
Philippe Perret <philippe.perret@yahoo.fr>
2016-09-26 13:59:25 +0200

    Poursuite inscription

--- objet/user/lib/module/signup2/helper/helper.rb
+++ objet/user/lib/module/signup2/helper/helper.rb
@@ -11,38 +11,24 @@ class << self
       'documents'     => {hname: 'Documents de présentation'},
       'confirmation'  => {hname: 'Confirmation inscription'}
     }.collect do |idstate, dstate|
-      selected = (state == idstate) ? 'selected' : nil
-      if state_done?(idstate)
-        href = "#{site.current_route.route}?signup[state]=#{idstate}"
-        dstate[:hname].in_a(href: href)
-      else
-        dstate[:hname]
-      end.in_div(class: "state #{selected}".strip)
+      div_class = ['state']
+      selected = (state == idstate)
+      selected && div_class << 'selected'
+      div =
+        if state_done?(idstate)
+          href = "#{site.current_route.route}?signup[state]=#{idstate}"
+          selected || div_class << 'done'
+          dstate[:hname].in_a(href: href)
+        else
+          dstate[:hname]
+        end
+      div.in_div( class: div_class.join(' ') )
     end.join.in_div(id: 'bandeau_states')
   end
 
   # Chargement de la vue (dans le dossier signup2/view/)
   def view relpath
     (folder_views + relpath).deserb(self)
-  end
-
-  def liste_modules_checkboxable
-    site.require_objet('abs_module')
-    cbs_modules = ''
-    AbsModule.each_instance(order: 'tarif ASC') do |amod|
-      href    = "abs_module/#{amod.id}/show"
-      cb_name = "modules[#{amod.id}]"
-      cb_id   = "modules-#{amod.id}"
-      mod_detail  = 'Détail'.in_a(href: href, class: 'small', target: :new).in_span(class: 'link')
-      mod_name    = amod.name.in_span(class: 'name')
-      mod_tarif   = "#{amod.tarif} €#{amod.type_suivi? ? ' / mois' : ''}".in_span(class: 'tarif')
-      mod_libelle = "#{mod_detail}#{mod_name} #{mod_tarif}"
-      mod_libelle = mod_libelle.in_checkbox(name: cb_name, id: cb_id)
-      cbs_modules << mod_libelle.in_li(class: 'absmodule', id: "absmodule-#{amod.id}")
-    end
-    # /fin boucle sur toutes les instances modules
-    cbs_modules = cbs_modules.in_ul(id:'abs_modules')
-    return cbs_modules
   end
 
 end #/ << self
--- objet/user/lib/module/signup2/pages_states.rb
+++ objet/user/lib/module/signup2/pages_states.rb
@@ -4,14 +4,18 @@ class << self
 
   def page_form_identite
     if (current_data = get_identite)
-      flash "J'ai pu récupérer les données de l'identité"
-      debug "current data identité : #{current_data.inspect}"
       param(user: current_data)
     end
     Signup.view('form_identite.erb')
   end
 
   def page_form_modules
+    if (current_data = get_modules)
+      debug "current modules : #{get_modules.inspect}"
+    else
+      current_data = Array.new
+    end
+    param(modules_checked: current_data)
     Signup.view('form_modules.erb')
   end
   def page_form_documents
